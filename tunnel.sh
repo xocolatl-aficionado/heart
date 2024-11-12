74946fe50c826a12da1a79d59792e730a35d9458
adi <adithyasudhan@gmail.com>
2024-11-12 14:00:57 -0330

    comment debugging lines

--- tunnel.sh
+++ tunnel.sh
@@ -4,10 +4,10 @@
 screen -dm bash -c 'ssh -v -R 80:localhost:5000 nokey@localhost.run > /tmp/tunnel_output.txt 2>&1'
 
 # Give the SSH tunnel some time to establish and produce output
-echo "Waiting for the SSH tunnel to establish..."
+# echo "Waiting for the SSH tunnel to establish..."
 sleep 10  # Wait 5 seconds or adjust as needed
 
-echo cat /tmp/tunnel_output.txt
+# echo cat /tmp/tunnel_output.txt
 # Check if the output file exists
 if [ -f /tmp/tunnel_output.txt ]; then
     # Capture the URL from the output
